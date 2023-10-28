2f3f3579264582e7e3833914bfb625735316ab05
Nathaly Angamarca <148395616+NathalyAngamarca@users.noreply.github.com>
2023-10-28 18:12:51 -0500

    Add files via upload

---
+++ mundo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+int main()
+{
+	 char nombre[20];
+	printf("Hola Mundo!!!");
+	 printf("\nIngresa tu nombre: ");
+    scanf("%s", nombre);
+    printf("Hola %s, bienvenido!\n", nombre);
+    
+    return 0;
+}
